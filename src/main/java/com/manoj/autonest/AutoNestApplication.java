7a75195a07a4fc8b940c630709088dba23faded9
Manoj Bharadwaj <bharadwaja1.618@gmail.com>
2024-08-16 11:41:16 +0530

    Dependency updation

---
+++ src/main/java/com/manoj/autonest/AutoNestApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.autonest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/autonest/AutoNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.autonest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
